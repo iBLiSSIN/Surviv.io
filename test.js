333e49ace3305cbf3b17bfd9e979a9ebfd844141
Me0w0 <71460119+Twist-ed@users.noreply.github.com>
2021-12-12 16:44:39 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,31 +1,23 @@
 (function() {
-    'use strict';
- 
-let shits = "";
- 
-const func = {
-    webpack_inject2: (w, e, get) => {
-        shits = get("03f4982a");
-    },
-};
- 
-if(typeof window.webpackJsonp === 'function') {
-    window.webpackJsonp([0], func, ["webpack_inject2"]);
-} else {
-    window.webpackJsonp.push([
-        ["webpack_inject2"],
-        func,
-        [["webpack_inject2"]]
-    ]);
-}
+        "use strict"
     
-Object.keys(shits).forEach(function(key2) {
-    if(key2.match(/tree/g)) {
-        shits[key2].img.sprite = "https://surviv.io/img/map/map-tree-res-02.svg";
-    } else if(key2.match(/bush/g)) {
-        shits[key2].img.scale = 0.16;
-    } else if(key2.match(/table/g)) {
-        shits[key2].img.sprite = "";
-    }
-})
+    let throwable = "";
+
+    const func = {
+        webpack_inject: (w, e, get) => {
+            throwable = get("0d807371");
+        },
+    };
+
+    if(typeof window.webpackJsonp === 'function') {
+        window.webpackJsonp([0], func, ["webpack_inject"]);
+    } else {
+        window.webpackJsonp.push([
+            ["webpack_inject"],
+            func,
+            [["webpack_inject"]]
+        ]);
+    };
+
+    throwable.players[m9_01].path = 'https://raw.githubusercontent.com/humphreygaming/survivcustomsounds/master/audio/guns/m9_01.mp3'
 })()
